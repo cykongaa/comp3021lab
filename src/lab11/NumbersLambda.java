0c7eb40a423c95128f09c0ba22d343f9d2365034
cykongaa <cykongaa@csl4wk17.win2k.cs.ust.hk>
2016-11-23 10:52:03 +0800

    lab11 update

--- src/lab11/NumbersLambda.java
+++ src/lab11/NumbersLambda.java
@@ -12,7 +12,8 @@ public class NumbersLambda {
 	// Find numbers with certain properties in a unified form
 	// The property is specified in Predicate
 
-	public static List<Integer> findNumbers(List<Integer> list, Predicate<Integer> predicate) {
+	public static List<Integer> findNumbers(List<Integer> list,
+			Predicate<Integer> predicate) {
 		List<Integer> results = new ArrayList<Integer>();
 		for (int n : list) {
 			if (predicate.test(n))
@@ -21,9 +22,10 @@ public class NumbersLambda {
 		return results;
 	}
 
-	public static List<Integer> calculateScore(List<Integer> list, Function<Integer, Integer> function) {
+	public static List<Integer> calculateScore(List<Integer> list,
+			Function<Integer, Integer> function) {
 		// TODO: Task 3
-	
+
 		List<Integer> results = new ArrayList<Integer>();
 		for (int n : list) {
 			results.add(function.apply(n));
@@ -33,7 +35,8 @@ public class NumbersLambda {
 
 	public static Function<Integer, Integer> policy() {
 		// TODO: Task 3
-		return x -> Boolean.compare(isOdd().test(x), false) * 1 + Boolean.compare(isPrime().test(x), false) * 2
+		return x -> Boolean.compare(isOdd().test(x), false) * 1
+				+ Boolean.compare(isPrime().test(x), false) * 2
 				+ Boolean.compare(isPalindrome().test(x), false) * 4;
 	}
 
@@ -45,23 +48,57 @@ public class NumbersLambda {
 
 	public static Predicate<Integer> isPrime() {
 		// TODO: Task 2
-		return x -> x % 2 != 0 && IntStream.range(2, x - 1).noneMatch(divisor -> x % divisor == 0);
+		// return x -> x % 2 != 0
+		// && IntStream.range(2, x - 1).noneMatch(
+		// divisor -> x % divisor == 0);
+
+		return x -> {
+			if (x % 2 == 0)
+				return false;
+			// if not, then just check the odds
+			for (int i = 3; i * i <= x; i += 2) {
+				if (x % i == 0)
+					return false;
+			}
+			return true;
+		};
 	}
 
 	public static Predicate<Integer> isPalindrome() {
 		// TODO: Task 2
-		return x -> Integer.toString(x).equals( new StringBuilder(Integer.toString(x)).reverse().toString());
+		// return x -> Integer.toString(x).equals( new
+		// StringBuilder(Integer.toString(x)).reverse().toString());
+		return x -> {
+			int origin = x;
+			int reverse = 0;
+
+			while (x > 0) {
+				int r = x % 10;
+				reverse = reverse * 10 + r;
+				x = x / 10;
+			}
+
+			if (origin == reverse)
+				return true;
+			else
+				return false;
+		};
+
 	}
 
-
 	public static void main(String[] args) {
-		List<Integer> numbers = Arrays.asList(480, 514, 484, 389, 709, 935, 328, 169, 649, 300, 685, 429, 243, 532, 308,
-				87, 25, 282, 91, 415);
+		List<Integer> numbers = Arrays.asList(480, 514, 484, 389, 709, 935,
+				328, 169, 649, 300, 685, 429, 243, 532, 308, 87, 25, 282, 91,
+				415);
 
-		System.out.println("The odd numbers are : " + findNumbers(numbers, isOdd()));
-		System.out.println("The prime numbers are : " + findNumbers(numbers, isPrime()));
-		System.out.println("The palindrome numbers are : " + findNumbers(numbers, isPalindrome()));
+		System.out.println("The odd numbers are : "
+				+ findNumbers(numbers, isOdd()));
+		System.out.println("The prime numbers are : "
+				+ findNumbers(numbers, isPrime()));
+		System.out.println("The palindrome numbers are : "
+				+ findNumbers(numbers, isPalindrome()));
 
-		System.out.println("The score of the all numbers are :" + calculateScore(numbers, policy()));
+		System.out.println("The score of the all numbers are :"
+				+ calculateScore(numbers, policy()));
 	}
 }
